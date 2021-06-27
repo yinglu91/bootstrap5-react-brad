6c5aed51b6f659742cfafe8d483c0e5172472960
ylu <ylu@ert.com>
2021-06-27 00:02:53 -0400

    refactor boxes

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,63 +1,50 @@
 import React from 'react';
 
+const boxesData = [
+  {
+    id: 1,
+    bgColor: 'dark',
+    title: 'Virtual',
+    btnBgColor: 'primary',
+  },
+  {
+    id: 2,
+    bgColor: 'secondary',
+    title: 'Hybrid',
+    btnBgColor: 'dark',
+  },
+  {
+    id: 3,
+    bgColor: 'dark',
+    title: 'In Person',
+    btnBgColor: 'primary',
+  },
+];
+
 const Boxes = () => {
   return (
     <section className="p-5">
       <div className="container">
         <div className="row text-center">
-          <div className="col-md">
-            <div class="card bg-dark text-light">
-              <div class="card-body text-center">
-                <div class="h1 mb-3">
-                  <i class="bi bi-laptop"></i>
+          {boxesData.map((box) => (
+            <div key={box.id} className="col-md">
+              <div className={`card bg-${box.bgColor} text-light`}>
+                <div className="card-body text-center">
+                  <div className="h1 mb-3">
+                    <i className="bi bi-laptop"></i>
+                  </div>
+                  <h3 className="card-title mb-3">{box.title}</h3>
+                  <p className="card-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Iure, quas quidem possimus dolorum esse eligendi?
+                  </p>
+                  <a href="#" className={`btn btn-${box.btnBgColor}`}>
+                    Read More
+                  </a>
                 </div>
-                <h3 class="card-title mb-3">Virtual</h3>
-                <p class="card-text">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Iure, quas quidem possimus dolorum esse eligendi?
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Read More
-                </a>
               </div>
             </div>
-          </div>
-
-          <div className="col-md">
-            <div class="card bg-secondary text-light">
-              <div class="card-body text-center">
-                <div class="h1 mb-3">
-                  <i class="bi bi-person-square"></i>
-                </div>
-                <h3 class="card-title mb-3">Hybrid</h3>
-                <p class="card-text">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Iure, quas quidem possimus dolorum esse eligendi?
-                </p>
-                <a href="#" class="btn btn-dark">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md">
-            <div class="card bg-dark text-light">
-              <div class="card-body text-center">
-                <div class="h1 mb-3">
-                  <i class="bi bi-people"></i>
-                </div>
-                <h3 class="card-title mb-3">In Person</h3>
-                <p class="card-text">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Iure, quas quidem possimus dolorum esse eligendi?
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
